e070409053bafbbbb71d3f9d8ca04f7e14b85341
selvendran.krishnan <selvendran.krishnan@rntbci.com>
2024-12-01 17:51:19 +0530

    parcel-commit and check Readme files

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement(
   "div",
   { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is h1 tag"),
+    React.createElement("h2", {}, "This is h2 tag"),
+  ]),
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "This is h1 tag"),
     React.createElement("h2", {}, "This is h2 tag"),
---
+++ README.md
@@ -0,0 +1,18 @@
+# Commands used 
+
+npm init
+npm i -D parcel
+npx parcel index.html
+
+npm i react
+npm i react-dom
+
+npx parcel build index.html
+
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+React.CreateElement()
+ReactDOM.createRoot()
+root.render()
