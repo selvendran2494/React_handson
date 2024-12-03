f6265194883b08579b16fb664a0e3f8909b7774d
selvendran.krishnan <selvendran.krishnan@rntbci.com>
2024-12-03 10:11:32 +0530

    task 1 : header component

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is h1 tag"),
-    React.createElement("h2", {}, "This is h2 tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is h1 tag"),
-    React.createElement("h2", {}, "This is h2 tag"),
-  ])
+import img1 from './assets/logo.png'
+import img2 from './assets/user-icon.jpg'
+
+const TitleComponent = () => (
+  <header className="header">
+    <div className="logo">
+      <img src={img1} alt="Logo" />
+    </div>
+    <div className="search-bar">
+      <input type="text" placeholder="Search..." />
+    </div>
+    <div className="user-icon">
+      <img src={img2} alt="User Icon" />
+    </div>
+  </header>
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<TitleComponent />);
+
--- README.md
+++ README.md
@@ -16,3 +16,30 @@ import ReactDOM from "react-dom/client"
 React.CreateElement()
 ReactDOM.createRoot()
 root.render()
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element
+const reactElement = <h1>This is React Element</h1>;
+
+// React Component with return
+const ReactComponent = () => {
+  return <h1>This is return ReactComponent</h1>;
+};
+
+// React Component without return
+const ReactComponentConvention = () => <h1>This is without return</h1>;
+
+//Component Composition
+const ReactComponentComposition = () => (
+  <div id="container">
+    <ReactComponent />
+    <ReactComponentConvention />
+    <h1>This is Component Composition</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ReactComponentComposition />);
