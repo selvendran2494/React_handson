d3e6a64dee9b95f9fde0d7cb7f255d81b65bbb49
selvendran.krishnan <selvendran.krishnan@rntbci.com>
2024-12-18 22:36:27 +0530

    app layout and some basic UI added

--- App.js
+++ App.js
@@ -1,22 +1,94 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import img1 from './assets/logo.png'
-import img2 from './assets/user-icon.jpg'
 
-const TitleComponent = () => (
-  <header className="header">
-    <div className="logo">
-      <img src={img1} alt="Logo" />
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <a href="/">
+          <img
+            style={{ width: "50px", height: " 50px" }}
+            src="https://t3.ftcdn.net/jpg/02/41/30/72/360_F_241307210_MjjaJC3SJy2zJZ6B7bKGMRsKQbdwRSze.jpg"
+            alt="My Logo"
+            className="logo-img"
+          />
+        </a>
+      </div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="card">
+      <img
+        src="https://t3.ftcdn.net/jpg/02/41/30/72/360_F_241307210_MjjaJC3SJy2zJZ6B7bKGMRsKQbdwRSze.jpg"
+        alt="Delicious Food"
+        className="card-img"
+      />
+      <div className="card-content">
+        <h2 className="card-title">Delicious Pizza</h2>
+        <p className="card-price">$12.99</p>
+      </div>
     </div>
-    <div className="search-bar">
-      <input type="text" placeholder="Search..." />
+  );
+};
+
+const Body = () => {
+  return (
+    <main className="main">
+      <div className="container">
+        {/* <h1 className="section-title">Popular Items</h1>             */}
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </main>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
     </div>
-    <div className="user-icon">
-      <img src={img2} alt="User Icon" />
-    </div>
-  </header>
-);
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<TitleComponent />);
-
+root.render(<AppLayout />);
