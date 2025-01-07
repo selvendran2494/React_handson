0e706b4a3fe06a438a70e60aa1b1d1dc9267b7dd
selvendran.krishnan <selvendran.krishnan@rntbci.com>
2025-01-07 22:31:38 +0530

    Refactor RestaurantCategory and Restaurantmenu components to manage item visibility state and improve code readability

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,37 +1,22 @@
-import { useState } from "react";
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({ data }) => {
-  const [showitems, setshowitems] = useState(false);
-  const [showArrow, setshowArrow] = useState(true);
+const RestaurantCategory = ({ data , showItems , setShowIndex }) => {
   const handleClick = () => {
-    setshowitems(!showitems);
-    setshowArrow(!showArrow);
+   setShowIndex()
   };
   return (
     <div
-      className="border-1 bg-gray-50  px-4 py-2 shadow-md my-2 cursor-pointer"
-      onClick={handleClick}
+      className="border-1 bg-gray-50  px-4 py-2 shadow-md my-2 cursor-pointer" onClick={handleClick}    
     >
       {/* 1.Header */}
       <div className="flex justify-between">
         <span className="font-bold">
           {data.title} ({data?.itemCards?.length})
-        </span>
-        {/* <span className="ml-2">
-          <i className="fas fa-chevron-down"></i>
-        </span>
-        <span className="ml-2">
-          <i className="fas fa-chevron-up"></i>
-        </span> */}
-        {showArrow ? (
-          <i className="fas fa-chevron-up"></i>
-        ) : (
-          <i className="fas fa-chevron-down"></i>
-        )}
+        </span>    
+        <i className="fas fa-chevron-up"></i>      
       </div>
       {/* 2.Body */}
-      {showitems && <ItemList item={data} />}
+      {showItems && <ItemList item={data} />}
     </div>
   );
 };
--- src/components/Restaurantmenu.js
+++ src/components/Restaurantmenu.js
@@ -3,9 +3,11 @@ import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantmenu from "../utils/useRestaurantmenu";
 import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
 const Restaurantmenu = () => {
   const { resid } = useParams();
+  const [showIndex, setShowIndex] = useState(0);
   const resdetails = useRestaurantmenu(resid);
 
   if (!resdetails) {
@@ -13,8 +15,15 @@ const Restaurantmenu = () => {
   }
   console.log("Res-details-res-menu", resdetails);
 
-  const { name, city, avgRating, costForTwoMessage, cuisines , totalRatingsString , sla } =
-    resdetails?.cards[2]?.card?.card.info;
+  const {
+    name,
+    city,
+    avgRating,
+    costForTwoMessage,
+    cuisines,
+    totalRatingsString,
+    sla,
+  } = resdetails?.cards[2]?.card?.card.info;
   const getCategories =
     resdetails?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) => {
@@ -34,62 +43,43 @@ const Restaurantmenu = () => {
     <div className="max-w-2xl mx-auto">
       <h1 className="font-extrabold text-xl py-5">{name}</h1>
       <div className="manual-shadow">
-      <div className="border-2 border-gray-300 rounded-lg shadow-lg px-3 py-3 max-w-2xl mx-auto inner-shadow">
-        {/* Banner Section */}
+        <div className="border-2 border-gray-300 rounded-lg shadow-lg px-3 py-3 max-w-2xl mx-auto inner-shadow">
+          {/* Banner Section */}
 
-        {/* Details Section */}
-        <div className="py-3">       
-          <div className="flex">          
-            <h2 className="font-bold">{avgRating} ({totalRatingsString} )</h2>
-            <li className="px-3">{costForTwoMessage}</li>
+          {/* Details Section */}
+          <div className="py-3">
+            <div className="flex">
+              <h2 className="font-bold">
+                {avgRating} ({totalRatingsString} )
+              </h2>
+              <li className="px-3">{costForTwoMessage}</li>
+            </div>
+            <h2 className="text-orange-600 text-sm font-bold underline cursor-pointer">
+              {cuisines.toString()}
+            </h2>
+            <h3 className="py-3">
+              <span className="font-bold text-sm">Outlet</span>
+              <span className="text-sm mx-1">{city}</span>
+            </h3>
+            <span className="font-bold text-sm">{sla?.slaString}</span>
           </div>
-          <h2 className="text-orange-600 text-sm font-bold underline cursor-pointer">{cuisines.toString()}</h2>
-          <h3 className="py-3">
-            <span className="font-bold text-sm">Outlet</span> 
-            <span className="text-sm mx-1">{city}</span>
-          </h3>      
-            <span className="font-bold text-sm">{sla?.slaString}</span>        
         </div>
-      </div>
       </div>
       {/* Menu Section */}
       <div className="menu-section">
         <h2 className="font-bold text-center">Menu Highlights</h2>
-
-        {/* <div className="flex flex-col max-w-2xl mx-auto">
-        <p className="mx-3 text-xl font-bold">Category</p>
-          {itemCards?.map((obj) => {
-            return (
-              <div
-                className="flex border-2 border-gray-200 shadow-lg p-2 m-2 rounded-lg"
-                key={obj.card.info.id}
-              >
-                <div className="flex-1">
-                  <h4 className="font-bold">{obj.card.info.name}</h4>
-                  <h4 className="font-bold">
-                    ₹{obj.card.info.price || obj.card.info.defaultPrice / 100}
-                  </h4>
-                  <p>{obj.card.info.description}</p>
-                </div>
-                <div className="flex-shrink-0 flex flex-col">
-                  <img
-                    src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${obj.card.info.imageId}`}
-                    alt="Dish"
-                    className="object-contain h-24 rounded-lg"
-                  />
-                  <button className="border-2 shadow-lg px-12 m-2 hover:bg-orange-300">
-                    Add
-                  </button>
-                </div>
-              </div>
-            );
-          })}
-        </div> */}
         <div className="flex flex-col max-w-2xl mx-auto">
-          {getCategories?.map((obj) => {
+          {getCategories?.map((obj, index) => {
             return (
               <div key={obj.card.card.title}>
-                <RestaurantCategory data={obj.card.card} />
+                <RestaurantCategory
+                  data={obj.card.card}
+                  showItems={index == showIndex && true}
+                  setShowIndex={() => {
+                    setShowIndex(index);
+                  }}
+                  // I can pass it as a call back function to update state variable of my parent.
+                />
               </div>
             );
           })}
